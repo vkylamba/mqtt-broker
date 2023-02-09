f63f41beb40207957cda735ede1678b6d5430ea0
Vikas Lamba <vikas13jun@gmail.com>
2023-02-09 19:06:26 +0100

    Fix start script

--- src/start_services.sh
+++ src/start_services.sh
@@ -1,7 +1,7 @@
 # Create CA file form env
-echo -e $ROOT_CA_CERT > /app/root_ca.crt
-echo -e $SERVER_CERT > /app/server.crt
-echo -e $SERVER_KEY >  /app/server.key
+echo -e $ROOT_CA_CERT > root_ca.crt
+echo -e $SERVER_CERT > server.crt
+echo -e $SERVER_KEY >  server.key
 
 mosquitto -c config/mosquitto.conf
 ./main
