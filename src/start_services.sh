ce2332cefa29b099845aec5567d03d963abe5e56
Vikas Lamba <vikas13jun@gmail.com>
2023-02-09 18:58:20 +0100

    Update config

--- src/start_services.sh
+++ src/start_services.sh
@@ -1,2 +1,8 @@
-mosquitto -c config/mosquitto.conf &
+# Create CA file form env
+mkdir /app/certs
+echo -e $ROOT_CA_CERT > /app/certs/root_ca.crt
+echo -e $SERVER_CERT > /app/certs/server.crt
+echo -e $SERVER_KEY >  /app/certs/server.key
+
+mosquitto -c config/mosquitto.conf
 ./main
